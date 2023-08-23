d87787b407ba55ea1054dd07fb7daa5a56499ac2
kirillskyeng <117906913+kirillskyeng@users.noreply.github.com>
2023-08-23 16:20:04 +0300

    Update item.py and tests

---
+++ config.py
@@ -0,0 +1,4 @@
+import os.path
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DICT_DIR = os.path.join(ROOT_DIR, 'src', 'items.csv')
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from config import DICT_DIR
+from src.item import Item
+
+
+def test_calculate_total_price():
+    obj1 = Item('помидорка', 15.0, 20)
+    assert obj1.calculate_total_price() == 300
+
+    obj2 = Item('луковичок', 20.0, 1)
+    Item.pay_rate = 0.8
+    obj2.apply_discount()
+    assert obj2.calculate_total_price() == 16.0
+
+    obj3 = Item("Смартфон", 10000, 20)
+    obj4 = Item("Ноутбук", 20000, 5)
+    Item.pay_rate = 0.8
+    obj3.apply_discount()
+    assert obj3.price == 8_000.0
+    assert obj4.price == 20_000.0
+
+
