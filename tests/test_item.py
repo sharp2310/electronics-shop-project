4b7a297ab3395cd6bc80984dac32e67dbec381e8
ilya <ilyamalahov038@gmail.com>
2024-01-22 21:35:58 +0300

    add

--- tests/test_item.py
+++ tests/test_item.py
@@ -46,3 +46,13 @@ def test_name():
 
     item2.name = 'СуперСмартфон'
     assert item2.name == 'СуперСмарт'
+
+
+def test_repr():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+
+def test_str():
+    item1 = Item("Смартфон", 10000, 20)
+    assert str(item1) == 'Смартфон'
